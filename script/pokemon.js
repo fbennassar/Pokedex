0941a9e493d1e9f434e3ab956e0795ba79a3cc71
franciscob <fbajo97@gmail.com>
2023-10-27 00:11:00 -0400

    Pokemon details 1.5 falta el mapa

--- script/pokemon.js
+++ script/pokemon.js
@@ -15,5 +15,32 @@ async function getPokemon() {
 
     document.getElementById('backSprite').src = response.data.sprites.back_default;
 
+    document.getElementById('height').innerHTML = response.data.height + ' cm';
+
+    document.getElementById('weight').innerHTML = response.data.weight + ' g';
+
+    var types = "";
+            response.data.types.forEach(element => {
+                if(types.length > 1){
+                    types += " / ";
+                }
+                types += element.type.name.charAt(0).toUpperCase() + element.type.name.slice(1) + " ";
+            });
+
+    document.getElementById('type').innerHTML = types;
+
+    var abilities = document.getElementById('abilities');
+
+    response.data.abilities.forEach(element => {
+        abilities.innerHTML += element.ability.name.charAt(0).toUpperCase() + element.ability.name.slice(1) + "<br>";
+    });
+
+    var moves = document.getElementById('moves');
+
+    response.data.moves.forEach(element => {
+        moves.innerHTML += element.move.name.charAt(0).toUpperCase() + element.move.name.slice(1) + "<br>";
+    });
+    
+
     // push de mi pc
 }
