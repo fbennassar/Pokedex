a645869a6b139705ad2b23912e7bddfab7f4e67b
Francisco Bennassar <fbajo97@gmail.com>
2023-10-28 14:48:58 -0400

    details DONE

--- script/pokemon.js
+++ script/pokemon.js
@@ -9,15 +9,27 @@ async function getPokemon() {
 
     console.log(response.data);
 
-    document.getElementById('name').innerHTML = response.data.name.charAt(0).toUpperCase() + response.data.name.slice(1);
+    document.getElementById('name').innerHTML = response.data.name.charAt(0).toUpperCase() + response.data.name.slice(1) + " #" + response.data.id;
 
     document.getElementById('sprite').src = response.data.sprites.front_default;
 
     document.getElementById('backSprite').src = response.data.sprites.back_default;
 
-    document.getElementById('height').innerHTML = response.data.height + ' cm';
+    var height = document.getElementById('height');
 
-    document.getElementById('weight').innerHTML = response.data.weight + ' g';
+    if(response.data.height < 10){
+        height.innerHTML = (response.data.height / 10).toFixed(1) + " m";
+    } else {
+        height.innerHTML = response.data.height / 10 + " m";
+    }
+
+    var weight = document.getElementById('weight');
+
+    if(response.data.weight < 10){
+        weight.innerHTML = (response.data.weight / 10).toFixed(1) + " kg";
+    } else {
+        weight.innerHTML = response.data.weight / 10 + " kg";
+    }
 
     var types = "";
             response.data.types.forEach(element => {
@@ -34,11 +46,27 @@ async function getPokemon() {
     response.data.abilities.forEach(element => {
         abilities.innerHTML += element.ability.name.charAt(0).toUpperCase() + element.ability.name.slice(1) + "<br>";
     });
+    
 
-    var moves = document.getElementById('moves');
+    const response2 = await axios.get(`https://pokeapi.co/api/v2/pokemon/${code}/encounters`);
 
+    var locationsList = document.getElementById('locationsList');
+
+    if(response2.data.length == 0){
+        locationsList.innerHTML = "No places found";
+    }
+    response2.data.forEach(element => {
+        locationsList.innerHTML += '<li>' + element.location_area.name.charAt(0).toUpperCase() + element.location_area.name.slice(1);
+    });
+
+    var tbody = document.getElementById('moves');
+
+    response.data.moves.sort(function(a, b) {
+        return b.version_group_details[0].level_learned_at - a.version_group_details[0].level_learned_at;
+    });
+    
     response.data.moves.forEach(element => {
-        moves.innerHTML += element.move.name.charAt(0).toUpperCase() + element.move.name.slice(1) + "<br>";
+        tbody.innerHTML += '<tr><td>' + element.move.name.charAt(0).toUpperCase() + element.move.name.slice(1) + '</td><td>' + element.version_group_details[0].level_learned_at + '</td></tr>';
     });
     
 
